feccafd9cd85758f92d9b2f2dc9d753689ddede9
Rana Essam <76626570+Ranaaessam@users.noreply.github.com>
2024-02-23 15:51:03 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using examinationsystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace examinationsystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
